4e3cbe7705b3515e2a50818cb809f71d7bf27c59
r1chardcs <mokryjvasa85@gmail.com>
2026-09-16 21:36:25 +0300

    Добавил возможность добавлять объекты удалять их и получить всех их по тегу

---
+++ Engine/objects/Rect.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dlllibstdntc on 16.09.2026.
+//
+
+#include "Rect.h"
---
+++ Engine/objects/Rect.h
@@ -0,0 +1,13 @@
+//
+// Created by dlllibstdntc on 16.09.2026.
+//
+
+#ifndef FASTENGINE_RECT_H
+#define FASTENGINE_RECT_H
+
+
+class Rect {
+};
+
+
+#endif //FASTENGINE_RECT_H
