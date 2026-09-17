01ec4c93242be94aad254d3bdbcea542a08ecba9
r1chardcs <mokryjvasa85@gmail.com>
2026-09-17 19:40:34 +0300

    Добавил возможность загрузить и отрисовать шрифт в формате .ttf

---
+++ Engine/UILayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dlllibstdntc on 17.09.2026.
+//
+
+#include "UILayer.h"
---
+++ Engine/UILayer.h
@@ -0,0 +1,16 @@
+//
+// Created by dlllibstdntc on 17.09.2026.
+//
+
+#ifndef FASTENGINE_UILAYER_H
+#define FASTENGINE_UILAYER_H
+
+
+
+class UILayer {
+
+};
+
+
+
+#endif //FASTENGINE_UILAYER_H
