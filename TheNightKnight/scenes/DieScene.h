485f8d56dddd2c75e94a045885e5c2e13f740a95
r1chardcs <mokryjvasa85@gmail.com>
2026-09-18 19:02:19 +0300

    Добавил поддержку загрузочных окон (появляются при длительной загрузке сцен)

---
+++ TheNightKnight/scenes/DieScene.h
@@ -0,0 +1,16 @@
+//
+// Created by dlllibstdntc on 18.09.2026.
+//
+
+#ifndef FASTENGINE_DIESCENE_H
+#define FASTENGINE_DIESCENE_H
+
+
+
+class DieScene {
+
+};
+
+
+
+#endif //FASTENGINE_DIESCENE_H
---
+++ TheNightKnight/scenes/LoadScene.h
@@ -0,0 +1,16 @@
+//
+// Created by dlllibstdntc on 18.09.2026.
+//
+
+#ifndef FASTENGINE_LOADSCENE_H
+#define FASTENGINE_LOADSCENE_H
+
+
+
+class LoadScene {
+
+};
+
+
+
+#endif //FASTENGINE_LOADSCENE_H
