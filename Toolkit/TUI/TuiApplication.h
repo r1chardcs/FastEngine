4d14b1fa4c7a89d69f3cdd6a81ee097c41813fc3
r1chardcs <mokryjvasa85@gmail.com>
2026-09-20 23:55:26 +0300

    Исправил Toolkit/CmakeList.txt ; Добавил Toolkit/Tui для консольных приложений. Начал писать FastBuild, система сборки для управления проектов с FastEngine

---
+++ Toolkit/TUI/TuiApplication.h
@@ -0,0 +1,16 @@
+//
+// Created by dlllibstdntc on 20.09.2026.
+//
+
+#ifndef FASTENGINE_TUIAPPLICATION_H
+#define FASTENGINE_TUIAPPLICATION_H
+
+
+
+class TuiApplication {
+
+};
+
+
+
+#endif //FASTENGINE_TUIAPPLICATION_H
